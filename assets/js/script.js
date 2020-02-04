3cdf9ff0da78758be33bd32ec5ca2b2744d038d6
Sittichai Chaikamol <nu@mcp.local>
2020-02-04 13:37:50 -0600

    nf

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,38 +10,8 @@
             $("tbody").append(tRow);
         };
 
-        // // search OMDB function
-        // var searchOMDB = function(cities) {
-        //     var queryURL = 'http://api.openweathermap.org/data/2.5/weather?q=city&units=metric&APPID=8796682f89508d38c2a35ab159e08782';
-        //     $.ajax({
-        //         url: queryURL,
-        //         medthod: "GET"
-        //     })
-        //     .then(function(response) {
-        //         createRow(response);
-        //         console.log(response)
-        //     })
-        // };
-        // searchOMDB("Houston");
-
-        // // $(document).ready(function() {
-        //     $(".search").click(function() {
-        //         var city = $("#city").val()
-        //         if(city != " "){
-        //             var queryURL = "http://api.openweathermap.org/data/2.5/box/city?bbox="+ city +"&units=metric" + "&APPID=8796682f89508d38c2a35ab159e08782";
-
-        //             $.ajax({
-        //             url: queryURL,
-        //             medthod: "GET"
-        //             });
-
-        //         }else{
-        //             $("#error").html("input your city")
-        //         }
-        //     });
-
         var searchOMDB = function(city) {
-        var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=Bangkok&units=metric&APPID=8796682f89508d38c2a35ab159e08782";
+        var queryURL = "http://api.openweathermap.org/data/2.5/weather?q="+ city + "&APPID=8796682f89508d38c2a35ab159e08782";
         $.ajax({
             url: queryURL,
             method: "GET"
@@ -50,16 +20,16 @@
         .then(function(response){
             console.log(queryURL);
             console.log(response);
+            createRow(response)
             $(".city").html("<h1>" + response.name + "");
             $(".temp").text("Temperature: " +response.main.temp + " F");
             $(".humidity").text("Humidity: " + response.main.humidity + " %");
             $(".wind").text("Wind Speed: " + response.wind.speed + " MPH");
             $(".table").text(response.name)
-            createRow(response)
 
             var tempF= (response.main.temp - 273.15) * 1.80 + 32; 
                 $("tempF").text("Temperature (Kelvin) " + tempF);
             
         })
     };
-    searchOMDB("Austin")
+    searchOMDB("Houston")
